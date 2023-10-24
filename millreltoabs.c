ba707f5fbef5b18bcd596ddd487107d21c2bb21e
Shane Feek <shane.feek@structuretec.local>
2023-10-24 13:26:42 -0400

    Added wheel movements
    Signed-off-by: Shane Feek <shane.feek@structuretec.local>

--- millreltoabs.c
+++ millreltoabs.c
@@ -3,26 +3,57 @@
 #include <math.h>
 #include "ghcommon.h"
 
+char *wheel_position (double x, double y, double dt)
+{
+    int xt,yt;
+    int xr,yr;
+    char *st = NULL;
+
+    xt = (int)trunc(x/dt);
+    xr = (int)round((x / dt - xt) * dt * 1000);
+    yt = (int)trunc(y/dt);
+    yr = (int)round((y / dt - yt) * dt * 1000);
+
+    sprintf_string(&st,"(%dT + %d, %dT + %d)", xt,xr,yt,yr);
+
+    return st;
+}
+
 int main (void)
 {
-    double sx,sy,a,ra,d,dx,dy;
+    double sx,sy,a,ra,d,dx,dy,dt;
+    char *wp = NULL;
 
+    dt = get_double("\nEnter Distance Per Turn of Wheel: ");
     sx = get_double("\nEnter Starting X Position: ");
     sy = get_double("\nEnter Starting Y Position: ");
 
+    
     while(TRUE)
     {
-        a = get_double("\n\n\nEnter Angle: ");
-        d = get_double("\nEnter Distance (0 to Quit): ");
-
-        if (d == 0.0) break;
+        d = get_double("\n\n\nEnter Distance (0 to Quit): ");
+        if (d <= 0.0) break;
+        
+        a = get_double("\nEnter Angle: ");
+       
+        wp = wheel_position(sx,sy,dt);
+        printf("\n\nStarting at \tX = %3.3f   Y = %3.3f   %s", sx,sy,wp);
+        free_malloc(wp);
 
         ra = PI * a / 180.0;
 
         dx = sx + d * sin(ra);
         dy = sy - d * cos(ra);
 
-        printf ("\n\nX = %3.4f    Y = %3.4f", dx, dy);
+        if (dx < 0.0 || dy < 0.0) 
+        {
+            printf("\n\nError... Outside of 0,0 bounds!");
+            continue;
+        }
+
+        wp = wheel_position(dx,dy,dt);
+        printf ("\n\nEnding at \tX = %3.3f   Y = %3.3f   %s", dx, dy,wp);
+        free_malloc(wp);
 
         sx = dx;
         sy = dy;
